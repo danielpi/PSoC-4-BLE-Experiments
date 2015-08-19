5539aa053eae67cb1aa7368dd8a3bb5ba3025548
Daniel Pink <dr.pink@gmail.com>
2015-08-19 15:16:18 +1000

    Working PSoC and Mac applications for the FindMe profile.

---
+++ Project 001 FindMe/PSoC/FindME/FindME.cydsn/main.c
@@ -0,0 +1,93 @@
+/* ========================================
+ *
+ * Copyright Electronic Innovations, 2015
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <project.h>
+//#include "ias.h"
+//#include "common.h"
+
+#define NO_ALERT           (0u)
+#define MILD_ALERT         (1u)
+#define HIGH_ALERT         (2u)
+
+#define NO_ALERT_COMPARE   (0u)
+#define MILD_ALERT_COMPARE (250u)
+#define HIGH_ALERT_COMPARE (500u)
+
+// Function Prototypes
+void StackEventHandler(uint32 eventCode, void *eventParam);
+void IasEventHandler(uint32 eventCode, void *eventParam);
+void HandleAlertLEDs(uint8 status);
+
+
+int main()
+{
+    CyGlobalIntEnable;   /* Enable global interrupts */
+    
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    
+
+    CyBle_Start( StackEventHandler );
+    PWM_1_Start();
+    CyBle_IasRegisterAttrCallback(IasEventHandler);
+    
+    while(1)
+    {
+        /* Place your application code here */
+        CyBle_ProcessEvents();
+    }
+}
+
+void StackEventHandler( uint32 eventCode, void *eventParam )
+{
+    switch( eventCode )
+    {
+        case CYBLE_EVT_STACK_ON:
+        case CYBLE_EVT_GAP_DEVICE_DISCONNECTED:
+            CyBle_GappStartAdvertisement( CYBLE_ADVERTISING_FAST );
+            break;
+
+        default:
+            break;
+    }
+}
+
+void IasEventHandler(uint32 eventCode, void *eventParam)
+{
+    uint8 alertLevel;
+    
+    switch(eventCode) {
+        case CYBLE_EVT_IASS_WRITE_CHAR_CMD:
+            // Extract the alert level
+            CyBle_IassGetCharacteristicValue(CYBLE_IAS_ALERT_LEVEL, sizeof(alertLevel), &alertLevel);
+            HandleAlertLEDs(alertLevel);
+            break;
+        default:
+            break;
+    }
+}
+
+void HandleAlertLEDs(uint8 status) {
+    switch(status) 
+    {
+        case NO_ALERT:
+            PWM_1_WriteCompare(NO_ALERT_COMPARE);
+            break;
+
+        case MILD_ALERT:
+            PWM_1_WriteCompare(MILD_ALERT_COMPARE);
+            break;
+            
+        case HIGH_ALERT:
+            PWM_1_WriteCompare(HIGH_ALERT_COMPARE);
+            break;                
+    }
+        
+}
--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 This set of projects is an attempt to use Cypress Semiconductors [100 projects in 100 days](http://www.cypress.com/?id=5509) as a basis for getting to know the [PSoC 4 BLE device](http://www.cypress.com/psoc4ble/) as well as Cocoa BLE programming. My plan is to try and re-create each project by myself (with the backup of being able to peruse a working project from [github](https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days). In addition I'll try to implement custom Mac or iOS projects that can interact with the dev board where appropriate.
 
 Let's see how I go...
+
+### Project 001 Find Me
+The first project looks at how to make a device that can call out if it is lost. This is your typical location tile that you put on your keychain and when you press a button on the phone it makes a noise and flashes a light to make it easier to find. This functionality is built in to the BLE spec as the [Find Me Profile](https://developer.bluetooth.org/TechnologyOverview/Pages/FMP.aspx). 
+
+The profile details the requirements for a locator and target devices. The main requirement is that the target has an Immediate Alert Service. 
+
+One thing I found interesting is that the Immediate Alert Service has an alert level that is write only. So the locator can set the alert level but it cannot interrogate the target to see what the alert level is. Interesting choice. I would have thought that being able to confirm the current alert level would be important however I guess this makes the task of complying with the spec easier for the target which is likely to be a very low powered device.
+- [Blog post](http://www.cypress.com/blog/100-projects-100-days/project-001-ble-find-me)
+- [Github](https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day001_Find_Me)
