00d9236ac0c5e81abd62954b9cc4b111b8d60fb8
Daniel Pink <dr.pink@gmail.com>
2015-09-23 14:28:55 +1000

    Not sure.

--- Project 003 Custom Profile CapSense RGB LED/PSoC/CapSenseRGB/CapSenseRGB.cydsn/main.c
+++ Project 003 Custom Profile CapSense RGB LED/PSoC/CapSenseRGB/CapSenseRGB.cydsn/main.c
@@ -10,11 +10,20 @@
  * ========================================
 */
 #include <project.h>
+#include <main.h>
 
 #define TRUE 	(1)
 #define FALSE 	(0)
 
 uint8 deviceConnected = FALSE;
+
+#define RGB_LED_MAX_VAL	(255)
+
+#define RED_INDEX 		(0)
+#define GREEN_INDEX 	(1)
+#define BLUE_INDEX 		(2)
+#define INTENSITY_INDEX (3)
+uint8 rgbLedData[4]
 
 
 int main()
@@ -58,8 +67,8 @@ void initializeSystem() {
 	
 	
 	// Init CapSense
-	CapSense_Start();
-	CapSense_InitializeAllBaselines();
+	Slider_Start();
+	Slider_InitializeAllBaselines();
 }
 
 void bluetoothEventHandler(uint32 event, void * eventParam) {
@@ -67,14 +76,14 @@ void bluetoothEventHandler(uint32 event, void * eventParam) {
     	case CYBLE_EVT_STACK_ON:
 		case CYBLE_EVT_GAP_DEVICE_DISCONNECTED:
 			// Start advertising and enter discoverable mode
-			CyBle_GappStartAdvertisment(CYBLE_ADVERTISING_FAST);
+			CyBle_GappStartAdvertisement(CYBLE_ADVERTISING_FAST);
 			break;
 			
 		case CYBLE_EVT_GAPP_ADVERTISEMENT_START_STOP:
 			// When advertising stops and we aren't connected to
 			// to a computer we want to restart advertising
 			if (CYBLE_STATE_DISCONNECTED == CyBle_GetState()) {
-				CyBle_GappStartAdvertisment(CYBLE_ADVERTISING_FAST);
+				CyBle_GappStartAdvertisement(CYBLE_ADVERTISING_FAST);
 			}
 			break;
 			
@@ -95,4 +104,31 @@ void bluetoothEventHandler(uint32 event, void * eventParam) {
 			
     }
 }
+
+void UpdateRGBled(void) {
+	// Local variables to calculate the color components from RGB received data
+	uint8 debug_red;
+	uint8 debug_green;
+	uint8 debug_blue;
+	uint8 intensity_divide_value = rgbLedData[INTENSITY_INDEX];
+	
+	debug_red = (uint8)(((uint16)rgbLedData[RED_INDEX] * intensity_divide_value) / 255);
+	debug_green = (uint8)(((uint16)rgbLedData[GREEN_INDEX] * intensity_divide_value) / 255);
+	debug_blue = (uint8)(((uint16)rgbLedData[BLUE_INDEX] * intensity_divide_value) / 255);
+	
+	// Update the density value of the PrISM module for color control
+	RedGreenPrISM_WritePulse0(RGB_LED_MAX_VAL - debug_red);
+    RedGreenPrISM_WritePulse1(RGB_LED_MAX_VAL - debug_green);
+    BluePrISM_WritePulse0(RGB_LED_MAX_VAL - debug_blue);
+	
+	// Update RGB control handle with new values
+	rgbHandle.attrHandle = RGB_LED_CHAR_HANDLE;
+	rgbHandle.value.val = RGBledData;
+	rgbHandle.value.len = RGB_CHAR_DATA_LEN;
+	rgbHandle.value.actualLen = RGB_CHAR_DATA_LEN;
+	
+	// Send updated RGB control handle as attribute for read by central device 
+	CyBle_GattsWriteAttributeValue(&rgbHandle, FALSE, &cyBle_connHandle, FALSE);  
+}
+
 /* [] END OF FILE */
---
+++ Project 003 Custom Profile CapSense RGB LED/PSoC/CapSenseRGB/CapSenseRGB.cydsn/main.h
@@ -0,0 +1,18 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+void initializeSystem(void);
+void bluetoothEventHandler(uint32 event, void * eventParam);
+void UpdateRGBled(void)
+
+
+/* [] END OF FILE */
